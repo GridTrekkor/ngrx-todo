b8fa08b2ff7b5fb6845eca1bc79f40dd2693d0ad
Jonathan Osborne <josborne@itm21st.com>
2018-01-19 13:40:39 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './reducers/todo.reducer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({ todoReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
